b80e22df1e5356d78537ae5f177161ef49b96831
Ariel Spear <ariel.spear@idexpertscorp.com>
2015-01-15 15:59:50 -0800

    Forgot to commit step 7 stuff.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,23 +3,19 @@
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
 
-describe('PhoneListCtrl', function(){
-  var scope, ctrl, $httpBackend;
-
-  // Load our app module definition before each test.
   beforeEach(module('phonecatApp'));
 
-  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-  // This allows us to inject a service but then attach it to a variable
-  // with the same name as the service in order to avoid a name conflict.
-  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('phones/phones.json').
-        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl, $httpBackend;
 
-    scope = $rootScope.$new();
-    ctrl = $controller('PhoneListCtrl', {$scope: scope});
-  }));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json').
+          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneListCtrl', {$scope: scope});
+    }));
 
 
     it('should create "phones" model with 2 phones fetched from xhr', function() {
@@ -33,6 +29,28 @@ describe('PhoneListCtrl', function(){
 
     it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('age');
+    });
+  });
+
+
+  describe('PhoneDetailCtrl', function(){
+    var scope, $httpBackend, ctrl;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
+
+      $routeParams.phoneId = 'xyz';
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+    }));
+
+
+    it('should fetch phone detail', function() {
+      expect(scope.phone).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phone).toEqual({name:'phone xyz'});
     });
   });
 });
